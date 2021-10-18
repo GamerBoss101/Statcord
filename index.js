a4b735adcc4dbbd0d8d9d321e0b629dadeaa842e
KorbsStudio <KorbsStudio@OnMail.com>
2021-10-18 01:59:54 -0400

    Use Acrylic

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function createWindow() {
         title: 'Statcord',
         icon: __dirname + '/icon.png',
         blur: true,
-        blurType: "blurbehind",
+        blurType: "acrylic",
         webPreferences: {
           nodeIntegration: true,
           contextIsolation: false
